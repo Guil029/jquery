64c6689bc9fa8d51deb9a8094dc490f73a984fc7
guigo <149022375+Guil029@users.noreply.github.com>
2024-01-18 07:31:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+$ (document).ready(function(){
+
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const endereçoNovaImagem = $('#endereço-img-nova').val();
+        const novoItem = $('<li style="display:none"></li>');
+        $(`<img  src="${endereçoNovaImagem}" />`).appendTo(novoItem);
+
+        $(`<div class="overlay-imagem-link">
+            <a href="${endereçoNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+        </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereço-imagem-nova').val('')
+    })
+})
+1
+
+
